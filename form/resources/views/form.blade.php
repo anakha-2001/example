3749b1b385a43ae14f58b8cbde6d76b22f7c27a9
Anakha Ajkumar <anakhaajkumar123@gmail.com>
2023-09-18 14:42:28 +0530

    trial

---
+++ form/resources/views/form.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Form</h1>
+    <form action="{{route('submit.form')}}"method="POST" enctype="multipart/form-data">
+        @csrf
+        <label for="name">Full Name</label>
+        <input type="text" id="name" name="name">
+        <label for="age">Age</label>
+        <input type="text" name="age" id="age">
+        <label for="date">Date</label>
+        <input type="text" name="date" id="date">
+        <input type="file" name="image" id="image">
+        <input type="submit">
+</body>
+</html>
